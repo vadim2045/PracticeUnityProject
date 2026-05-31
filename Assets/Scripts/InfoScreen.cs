5197192b82f6b631b7c17ce60f8334762bf762b1
vadim2045 <vadimdah2@gmail.com>
2026-05-31 16:35:24 +0300

    Crash_fix

--- Assets/Scripts/InfoScreen.cs
+++ Assets/Scripts/InfoScreen.cs
@@ -4,7 +4,6 @@ using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using static UnityEditor.Progress;
 
 public class InfoScreen : MonoBehaviour, IPointerExitHandler
 {
